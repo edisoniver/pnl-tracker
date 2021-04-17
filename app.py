c24477665c89e21ce1cdb297639ff0ab349d0e21
edisoniver <edisoniverbotor@gmail.com>
2021-04-18 00:50:40 +1000

    added basic database foundation

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 from flask import Flask, request, render_template 
 import sqlite3
 import ccxt 
+from datetime import date
 app = Flask(__name__)
 
 
@@ -12,27 +13,55 @@ connection = sqlite3.connect('trades.db')
 global cur
 cursor = connection.cursor()
 
+cursor.execute("CREATE TABLE IF NOT EXISTS trades(id INTEGER PRIMARY KEY AUTOINCREMENT, coin TEXT, amount FLOAT, entry FLOAT, date TEXT)") 
 def main():
-    coin = str(input("What is coin"))
-    amount = float(input("How much"))
-    entry = float(input("What price"))
-    
-    sql_insert(coin, amount, entry)
+    coin = str(input("What is coin? "))
+    amount = float(input("How many? "))
+    entry = float(input("What price? "))
+    today = date.today()
+    datenow = today.strftime("%b-%d-%Y")
+ 
+    sql_insert(coin, amount, entry, datenow)
     coin_info(coin)
 
 
 def coin_info(coin):
 
-    rows = cursor.execute("SELECT coin, amount, entry FROM trades").fetchall() 
+    rows = cursor.execute("SELECT id, coin, amount, entry, date FROM trades").fetchall() 
     print(rows)
 
-def sql_insert(coin, amount, entry):
-    
-    insert = 'INSERT INTO trades VALUES("{0}", {1}, {2})'.format(coin, float(amount), float(entry))
+def sql_insert(coin, amount, entry, newdate):
+    # --------- # 
+    # Checks for existing value in the database 
+    # If it is not, then add value to the database
+    # If it is, then retrieve values, drop the table
+    # Then add it to the database
+    # For variable amount -> ( amount + amount ) = new amount
+    # For variable entry -> (entry + entry / 2) = new entry or averaged entry
 
-    cursor.execute(insert) 
-    connection.commit()
+    today = date.today()
+    datenow = today.strftime("%b-%d-%Y")
+    check_for_existing = cursor.execute("SELECT id, coin FROM trades WHERE coin  = '{0}'".format(coin)).fetchone()
 
+    if check_for_existing == None:
+        insert = 'INSERT INTO trades(coin, amount, entry, date) VALUES("{0}", {1}, {2}, "{3}")'.format(coin, float(amount), float(entry), datenow)
+        cursor.execute(insert) 
+        connection.commit()
+    else:
+        existing_value =  cursor.execute("SELECT coin, amount, entry  FROM trades WHERE coin = '{0}'".format(coin)).fetchone()
+        amount = amount + existing_value[1]
+        entry = (entry + existing_value[2]) / 2
+        if newdate == datenow:
+            pass
+        else:
+            newdate = newdate + ' | ' + datenow 
+
+        print('EXISTING VALUE', existing_value, amount, entry, newdate)
+
+        connection.commit()
+        # Drop a row then add new table. 
+        # OR 
+        # Update table with unique ID.
 main()
 #
 #@app.route("/")
